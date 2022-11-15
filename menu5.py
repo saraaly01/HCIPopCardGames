a944953485cb23b98c068bce11900dfb3eec179a
kiliban1 <44709833+kiliban1@users.noreply.github.com>
2022-11-15 10:32:10 -0500

    User Interface Example
    This is an example of a user interface menu that does not have the message scrolling in the terminal saying "The event loop is already running". NOTE: comment out the line main() at the very last line of the file theGame21.py before running this menu file, so the program does not run before you click on the button.

---
+++ menu5.py
@@ -0,0 +1,21 @@
+import theGame21
+from tkinter import *
+
+window=Tk()
+
+window.title("Running Python Script")
+window.geometry('500x200')
+
+def run_21():
+    theGame21.main()
+
+def run_war():
+    print("Need code for the game War")
+
+btn = Button(window, text="The Game 21", bg="black", fg="white",command=run_21)
+btn.grid(column=0, row=0)
+
+btn2 = Button(window, text="War", bg="black", fg="white",command=run_war)
+btn2.grid(column=0, row=2)
+
+window.mainloop()
