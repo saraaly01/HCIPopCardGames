71f5bd882e5c9f0d123470d932dc35c826f5e7a1
saraaly01 <aly.sara01@gmail.com>
2022-12-05 15:57:31 -0500

    visual feedback for menu

--- Final/menu.py
+++ Final/menu.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from customtkinter import *
 import speech_recognition as sr
 import os, sys, time
 from gtts import gTTS
@@ -8,12 +7,14 @@ from GUI21 import main21
 from warGUI import intialize
 from globalFunctions import *
 
-global rootMenu,  outPutMenu, var, endMenu, btnWar, btn21
+global rootMenu,  outPutMenu, endMenu, btnWar, btn21, warning
 endMenu = 0
 outPutMenu = queue.Queue()  # output queue will hold messages that a thread will output with voice
 def outPutAudioMenu():
-    global outPutMenu, endMenu
+    global outPutMenu, endMenu, var
     while True:
+        if var.get() != 0:
+            warning.destroy()
         if endMenu:
             return
         while outPutMenu.empty() == False:
@@ -29,7 +30,7 @@ def outPutAudioMenu():
 
 #Thread function- changes the audio setting and chooses game based on user voice input
 def audioListenerMenu():
-    global endMenu
+    global endMenu, warning
     r = sr.Recognizer()
     mic = sr.Microphone()
     while True:
@@ -41,10 +42,13 @@ def audioListenerMenu():
         if msg == "audio":
             #audioChoice = "Vaudio" #!!differentiate audio from button and audio from voice
             var.set(1)
-            #outputCurSetting()
+            warning.destroy()
+            outPutMenu.put("Audio Selected")
         elif msg == "silent" or msg == "silence":
             #audioChoice = "Vsilent" #!!differentiate audio from button and audio from voice
             var.set(2)
+            warning.destroy()
+            outPutMenu.put("Silent Selected")
         if msg == "first":
             btn21.invoke()
             return
@@ -62,10 +66,12 @@ def quitMenu():
 
 
 def choose_21():
-    global outputMenu, endMenu, rootMenu
+    global outputMenu, endMenu, rootMenu, warning
     value = var.get()
     if value == 0:
-        outPutMenu.put("No audio choice selected. Say 'audio' or 'silent' or click the radio buttons")# put visually
+        outPutMenu.put("No audio choice selected. Say 'audio' or 'silent' or click the radio buttons")
+        warning = Label(rootMenu, text= "No audio choice selected. Say 'audio' or 'silent' or click the radio buttons", font=("Arial", 10))
+        warning.place(relx= .1, rely= .25)
     else:
         rootMenu.destroy()
         endMenu = 1
@@ -73,10 +79,12 @@ def choose_21():
     return
 
 def choose_war():
-    global outPutMenu, endMenu
+    global outPutMenu, endMenu, warning
     value = var.get()
     if value == 0:
         outPutMenu.put("No audio choice selected. Say 'audio' or 'silent' or click the radio buttons")
+        warning = Label(rootMenu, text= "No audio choice selected. Say 'audio' or 'silent' or click the radio buttons", font=("Arial", 10))
+        warning.place(relx= .1, rely= .25)
     else:
         rootMenu.destroy()
         endMenu = 1
@@ -87,9 +95,7 @@ def main():
     global outPutMenu, endMenu, var, rootMenu, btnWar, btn21
     listenerThread = threading.Thread(target=audioListenerMenu)
     listenerThread.start()
-    audioSpeakerThread = threading.Thread(target=outPutAudioMenu)
-    audioSpeakerThread.start()
-    outPutMenu.put("Welcome to PopCard Games. At anytime say silent or audio to choose game play mode. Then say first to choose 21 and second to choose war")
+  
     rootMenu = Tk()
     rootMenu.title('PLAY')
     rootMenu['background']='#8B0000'
@@ -101,10 +107,16 @@ def main():
     rootMenu.geometry("%dx%d" % (window_width,  window_height))
     rootMenu.state("zoomed")
     ##THREAD SECTION
-    gameTitle = Label(rootMenu, text= "POPCARD GAMES", font=("Cooper Black", 80))
+    gameTitle = Label(rootMenu, text= "POPCARD GAMES", font=("Cooper Black", 50))
     gameTitle.place(relx= .2, rely= 0)
     var = IntVar()
-
+    var.set(0)
+    audioSpeakerThread = threading.Thread(target=outPutAudioMenu)
+    audioSpeakerThread.start()
+    outPutMenu.put("Welcome to PopCard Games. At anytime say silent or audio to choose game play mode. Then say first to choose 21 and second to choose war")
+    menuInstruction = Label(rootMenu, text= "Welcome to PopCard Games. At anytime say silent or audio to choose game play mode or click the respective button.\
+    THEN say first to choose 21 and second to choose war.", font=("Arial", 10))
+    menuInstruction.place(relx= .1, rely= .2)
     audioButton = Radiobutton(rootMenu, text ="Audio", font=("Helvetica", 50), variable=var, value=1)
     audioButton.place(relx = 0.05, rely = .3)
     slientButton = Radiobutton(rootMenu, text ="Silent",  font=("Helvetica", 50), variable=var, value=2)
